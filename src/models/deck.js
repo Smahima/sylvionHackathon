3546c691a885f8c596e2699eb17a0336195eb7f9
Shannon <reidy.pf@gmail.com>
2017-10-04 10:08:54 -0500

    both decks shuffle and output

--- src/models/deck.js
+++ src/models/deck.js
@@ -1,40 +1,42 @@
 let SylvanDeck = {
   theCards: [{
       cardName: "tiny fountain",
-      cardNumber: 3
+      cardNumber: 3,
+      baseCardNumber: 3,
       power: 1
     },
     {
       cardName: "small fountain",
-      cardNumber: 3
+      cardNumber: 3,
+      baseCardNumber: 3,
       power: 2,
     },
     {
       cardName: "medium fountain",
-      cardNumber: 3
+      cardNumber: 3,
+      baseCardNumber: 3,
       power: 3
     },
     {
       cardName: "large fountain",
-      cardNumber: 3
+      cardNumber: 3,
+      baseCardNumber: 3,
       power: 4
     }
   ],
   cardTypes: 4,
   shuffleDeck: function() {
     let shuffledDeck = [];
-    let currentDraw = this.theCards[Math.floor(Math.random() * this.cardTypes)];
+    while(this.getCardTotal() !== 0){
+    let currentDraw = this.theCards[Math.floor(Math.random() * this.theCards.length)];
     if (currentDraw.cardNumber !== 0) {
-      aDeck.cardTotal--;
       currentDraw.cardNumber--;
-      shuffledDeck.push(currentDraw.cardName);
-
-    } else if (aDeck.cardTotal === 0) {
-
+      shuffledDeck.push(currentDraw);
     } else {
-      shuffleDeck();
+      this.shuffleDeck();
     }
-
+  }
+  return shuffledDeck;
   },
   getCardTotal: function() {
     let cardSum = 0;
@@ -42,6 +44,68 @@ let SylvanDeck = {
       cardSum += cardtype.cardNumber
     })
     return cardSum;
+  },
+  resetDeck: function() {
+    this.theCards.forEach(cardtype => {
+      cardtype.cardNumber = cardtype.baseCardNumber;
+    })
   }
 
 };
+
+let FireDeck = {
+  theCards: [{
+      cardName: "tiny fire",
+      cardNumber: 3,
+      baseCardNumber: 3,
+      power: 0
+    },
+    {
+      cardName: "small fire",
+      cardNumber: 3,
+      baseCardNumber: 3,
+      power: 1,
+    },
+    {
+      cardName: "medium fire",
+      cardNumber: 3,
+      baseCardNumber: 3,
+      power: 2
+    },
+    {
+      cardName: "large fire",
+      cardNumber: 3,
+      baseCardNumber: 3,
+      power: 3
+    }
+  ],
+  shuffleDeck: function() {
+    let shuffledDeck = [];
+    while(this.getCardTotal() !== 0){
+    let currentDraw = this.theCards[Math.floor(Math.random() * this.theCards.length)];
+    if (currentDraw.cardNumber !== 0) {
+      currentDraw.cardNumber--;
+      shuffledDeck.push(currentDraw);
+    } else {
+      this.shuffleDeck();
+    }
+  }
+  return shuffledDeck;
+  },
+  getCardTotal: function() {
+    let cardSum = 0;
+    this.theCards.forEach(cardtype => {
+      cardSum += cardtype.cardNumber
+    })
+    return cardSum;
+  },
+  resetDeck: function() {
+    this.theCards.forEach(cardtype => {
+      cardtype.cardNumber = cardtype.baseCardNumber;
+    })
+  }
+
+};
+
+console.log(SylvanDeck.shuffleDeck());
+console.log(FireDeck.shuffleDeck());
