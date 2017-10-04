1de5d4acdaaacade3997d9f9adbf2ee4dae25d18
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-04 12:02:06 -0500

    add hand card

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -1,37 +1,26 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 export default class Deal extends Component {
-
   render() {
-
     return (
-        <div className="defense">
-
-            <div  className="discard">
-              <div className="card-block">
-                <h4 className="card-title">Discard Pile</h4>
-              </div>
-            </div>
-
-            <div className="deck">
-              <div className="card-block">
-              <h4 className="card-title">Defense Deck</h4>
-              </div>
-              <div>
-                <button className="deal">Deal</button>
-              </div>
-            </div>
-
-              <div className="play">
-              <div className="card-block">Hand to Play</div>
-              <div>
-                <button className="plant">Plant</button>
-              </div>
-            </div>
-
+      <div className="defense">
+        <div className="card-block">
+          <h4 className="card-title">Discard Pile</h4>
+          <div className="discard" />
         </div>
-
-      );
-    };
+        <div className="card-block">
+          <h4 className="card-title">Defense Deck</h4>
+          <div className="deck" />
+          <button className="deal">Deal</button>
+        </div>
+        <div className="card-block">
+          <h4 className="card-title">Hand to Play</h4>
+          <div className="play" />
+          <div className="play" />
+          <button className="plant">Plant</button>
+        </div>
+      </div>
+    );
   }
+}
