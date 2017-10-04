a0cab8348f05a7d65f74e981a889e1b59f53f988
Your name <Mahima Subramanian>
2017-10-04 09:41:01 -0500

    created defense deck shell and deal button

---
+++ src/components/Deal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+export default class Deal extends Component {
+
+  render() {
+    return (
+        <div>
+
+          <div className="deck">
+              <p>Defense Deck</p>
+          </div>
+
+          <button className="deal">Deal</button>
+
+        </div>
+      );
+    };
+  }
