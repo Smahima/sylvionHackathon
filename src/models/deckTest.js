ad150247f02aaa305dfff7829e103f0a9a4ab4ac
Shannon <reidy.pf@gmail.com>
2017-10-04 11:50:24 -0500

    data file can now be required

--- src/models/deckTest.js
+++ src/models/deckTest.js
@@ -1,2 +1,8 @@
 
-console.log(SylvanDeck.shuffleDeck());
+//probably a more specific path from wherever you are
+const decks = require('./deck.js')
+
+
+console.log(decks.SylvanDeck.shuffleDeck());
+//so we use requiredvariablname(decks).whichdeck.functionName
+//shuffleDeck will output an arraylist of card objects
