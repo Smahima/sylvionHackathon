432374c51062560e9acb953bf3069b94bf115666
Your name <Mahima Subramanian>
2017-10-04 10:30:14 -0500

    template for discard pile deck and play hand

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -4,16 +4,64 @@ import '../styles/App.css';
 export default class Deal extends Component {
 
   render() {
+
+  let defense = {
+    display: "flex",
+    border:"5px solid green"
+  }
+
+  let discard = {
+    border: "1px dotted grey",
+
+  }
+
+  let deck = {
+    border: "1px solid blue 20%",
+    marginLeft: "10px",
+  }
+
+  let play = {
+    border: "1px solid orange 20%",
+    marginLeft: "10px",
+  }
+
+
     return (
-        <div>
+        <div style={defense} className="defense">
 
-          <div className="deck">
-              <p>Defense Deck</p>
-          </div>
+            <div style={discard} className="discard">
+              <div className="card-block">
+                <h4 className="card-title">Discard Pile</h4>
+              </div>
+            </div>
 
-          <button className="deal">Deal</button>
+            <div style={deck} className="deck">
+              <div className="card-block">
+              <h4 className="card-title">Defense Deck</h4>
+              </div>
+
+              <div>
+                <button className="deal">Deal</button>
+              </div>
+            </div>
+
+            <div style={play} className="play">
+              <div className="card-title">Hand to Play
+                <div className="card-block">
+
+                  <div className="hand">
+                </div>
+                
+                </div>
+              </div>
+
+              <div>
+                <button className="plant">Plant</button>
+              </div>
+            </div>
 
         </div>
+
       );
     };
   }
